17a3576728f2a098132aa2324bf96bec8c9aa9d7
s9chroma <s9chroma@gmail.com>
2020-12-20 14:49:06 +0100

    Initial Commit

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+if not exist build mkdir build
+pushd build
+call cmake ..
+call cmake --build .
+if not errorlevel 1 call "Debug/LineSimp.exe"
+popd
---
+++ notes.md
@@ -0,0 +1,3 @@
+## 20/12/2020
+- Get CGAL working on both computers.
+- 
